40253940d3388bfd1961c58be06543aa3d7933bf
Giuseppe <giuseppe.dimartino96@hotmail.it>
2024-03-29 16:44:18 +0100

    include spring e uml

---
+++ code/src/main/java/com/group/cineMille/CineMilleApplication.java
@@ -0,0 +1,13 @@
+package com.group.cineMille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMilleApplication.class, args);
+	}
+
+}
